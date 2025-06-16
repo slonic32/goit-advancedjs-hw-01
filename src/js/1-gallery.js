2b0c6785d6f2347d0c98920589d9273b1d1ac1d1
Artem Hordiyevskyy <139376262+slonic32@users.noreply.github.com>
2025-06-16 21:00:17 +0300

    init 2
    init 2

---
+++ src/js/1-gallery.js
@@ -0,0 +1,28 @@
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// Change code below this line
+const galleryList = document.querySelector('ul.gallery');
+createGallery();
+let gallery = new SimpleLightbox('a.gallery__link', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function createGallery() {
+  const images = [];
+  for (let i = 0; i < galleryItems.length; i++) {
+    const image = `<li class="gallery__item">
+        <a class="gallery__link" href="${galleryItems[i].original}">
+            <img
+                class="gallery__image"
+                src="${galleryItems[i].preview}"
+                alt="${galleryItems[i].description}"
+            />
+        </a>
+    </li>`;
+    images.push(image);
+  }
+  galleryList.innerHTML = images.join('');
+}
